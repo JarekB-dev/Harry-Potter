2b48dda486c3cfba70130adfef5bc854dbfb3774
Jarek Bonk <jerrybonq@gmail.com>
2023-05-27 17:00:05 +0000

    add flipCard function to flip cards on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard;
+let secondCard;
+const cards = document.querySelectorAll('.memory-card');
+
+
+
+
+
 
 document.getElementById('button-enter').addEventListener('click', () => {
   document.getElementById('image-left').classList.add('transform-left');
   document.getElementById('image-right').classList.add('transform-right');
   document.getElementById('main-screen').style.display = "none";
-})
+});
+
+/**
+ * Function that adds flip class to clicked card
+ */
+function flipCard() {
+  // if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add('flip');
+        
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));

90b1abb4cbbdb5780b6112459f14a54b48de8a3b
Jarek Bonk <jerrybonq@gmail.com>
2023-05-27 17:21:53 +0000

    add name screen and logic to make it visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ document.getElementById('button-enter').addEventListener('click', () => {
   document.getElementById('image-left').classList.add('transform-left');
   document.getElementById('image-right').classList.add('transform-right');
   document.getElementById('main-screen').style.display = "none";
+  nameScreenLoad();
 });
 
 /**
@@ -32,3 +33,15 @@ function flipCard() {
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+/**
+ * Shows modal asking player to input their name.
+ */
+function nameScreenLoad() {
+  setTimeout( function() {
+    document.getElementById('name-screen').classList.add('opacity');
+    document.getElementById('name-screen').style.visibility = "visible";
+    document.getElementById('player-name').focus();
+  }, 1500);
+}
